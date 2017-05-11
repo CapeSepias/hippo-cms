b0a3d22b637f98f510247e80e8685ea16961e3e9
Marijan Milicevic <marijan.milicevic@bloomreach.com>
2017-05-11 14:36:52 +0200

    CMS-10722 Back port to 10.2: When using the search filter in the gallery (or image picker), I want to see thumbnails - fix css append model

--- gallery/frontend/src/main/java/org/hippoecm/frontend/plugins/gallery/ImageGalleryPlugin.java
+++ gallery/frontend/src/main/java/org/hippoecm/frontend/plugins/gallery/ImageGalleryPlugin.java
@@ -27,6 +27,7 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.ISortableDataProvider;
 import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.Model;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
@@ -61,8 +62,12 @@ public class ImageGalleryPlugin extends ExpandCollapseListingPlugin<Node> {
         setClassName(DocumentListColumn.DOCUMENT_LIST_CSS_CLASS);
 
         add(CssClass.append("image-gallery"));
-        add(CssClass.append(Model.of(mode == LIST ? "image-gallery-list" : "image-gallery-thumbnails")));
-
+        add(CssClass.append(new AbstractReadOnlyModel<String>() {
+            @Override
+            public String getObject() {
+                return mode == LIST ? "image-gallery-list" : "image-gallery-thumbnails";
+            }
+        }));
         addButton(new GalleryModeButton("listButton", LIST, Icon.LIST_UL));
         addButton(new GalleryModeButton("thumbnailsButton", THUMBNAILS, Icon.THUMBNAILS));
     }
