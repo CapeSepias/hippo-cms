131de26cd984f2143caa58116b0aed8050885386
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-06-06 12:53:53 -0400

    CMS-10753: changing visibility for custom code

--- automatic-export/repository/src/main/java/org/onehippo/cms7/autoexport/InternalLocationMapperEntries.java
+++ automatic-export/repository/src/main/java/org/onehippo/cms7/autoexport/InternalLocationMapperEntries.java
@@ -27,9 +27,10 @@ import java.util.regex.Pattern;
  */
 public class InternalLocationMapperEntries {
 
+    protected static final String NAME = "([^/\\u005B\\u005D\\|\\*]+(?:\\u005B\\d+\\u005D)?)";
+    protected static final String ANY = "(.*)";
+
     private static final List<Entry> ENTRIES = new ArrayList<Entry>();
-    private static final String NAME = "([^/\\u005B\\u005D\\|\\*]+(?:\\u005B\\d+\\u005D)?)";
-    private static final String ANY = "(.*)";
 
     static {
         // /hippo:namespaces/example
@@ -37,127 +38,131 @@ public class InternalLocationMapperEntries {
         String[] propertyPatterns = new String[] {"/hippo:namespaces/" + NAME + "/" + NAME};
         String contextNode = "/hippo:namespaces/$1";
         String file = "namespaces/$1.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hippo:namespaces/example/doctype
         nodePatterns = new String[] {"/hippo:namespaces/" + NAME + "/" + NAME, "/hippo:namespaces/" + NAME + "/" + NAME + "/" + ANY};
         propertyPatterns = new String[] {"/hippo:namespaces/" + NAME + "/" + NAME + "/" + ANY};
         contextNode = "/hippo:namespaces/$1/$2";
         file = "namespaces/$1/$2.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hst:hst/hst:sites
         nodePatterns = new String[] {"/hst:hst/hst:sites" + ANY};
         propertyPatterns = nodePatterns;
         contextNode = "/hst:hst/hst:sites";
         file = "hst/sites.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hst:hst/hst:hosts
         nodePatterns = new String[] {"/hst:hst/hst:hosts" + ANY};
         propertyPatterns = nodePatterns;
         contextNode = "/hst:hst/hst:hosts";
         file = "hst/hosts.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hst:hst/hst:configurations
         nodePatterns = new String[] {"/hst:hst/hst:configurations", "/hst:hst/hst:configurations/" + NAME};
         propertyPatterns = new String[] {"/hst:hst/hst:configurations/" + NAME, "/hst:hst/hst:configurations/" + NAME + "/" + NAME};
         contextNode = "/hst:hst/hst:configurations";
         file = "hst/configurations.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hst:hst/hst:configurations/project
         nodePatterns = new String[] {"/hst:hst/hst:configurations/" + NAME + "/" + NAME};
         propertyPatterns = new String[] {"/hst:hst/hst:configurations/" + NAME + "/" + NAME + "/" + NAME};
         contextNode = "/hst:hst/hst:configurations/$1/$2";
         file = "hst/configurations/$1/$2.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hst:hst/hst:configurations/project/hst:components|hst:pages|hst:abstractpages|hst:prototypepages
         nodePatterns = new String[] {"/hst:hst/hst:configurations/" + NAME + "/(hst:components|hst:pages|hst:abstractpages|hst:prototypepages)/" + NAME, "/hst:hst/hst:configurations/" + NAME + "/(hst:components|hst:pages|hst:abstractpages|hst:prototypepages)/" + NAME + "/" + ANY};
         propertyPatterns = new String [] {"/hst:hst/hst:configurations/" + NAME + "/(hst:components|hst:pages|hst:abstractpages|hst:prototypepages)/" + NAME + "/" + ANY};
         contextNode = "/hst:hst/hst:configurations/$1/$2/$3";
         file = "hst/configurations/$1/$2/$3.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hst:hst/hst:configurations/project
         nodePatterns = new String[] {"/hst:hst/hst:configurations/" + NAME + "/" + NAME + "/" + ANY};
         propertyPatterns = nodePatterns;
         contextNode = "/hst:hst/hst:configurations/$1/$2";
         file = "hst/configurations/$1/$2.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hst:hst/hst:blueprints
         nodePatterns = new String[] {"/hst:hst/hst:blueprints"};
         propertyPatterns = new String[] {"/hst:hst/hst:blueprints/" + NAME};
         contextNode = "/hst:hst/hst:blueprints";
         file = "hst/blueprints.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hst:hst/hst:blueprints/subsite
         nodePatterns = new String[] {"/hst:hst/hst:blueprints/" + NAME};
         propertyPatterns = new String[] {"/hst:hst/hst:blueprints/" + NAME + "/" + NAME};
         contextNode = "/hst:hst/hst:blueprints/$1";
         file = "hst/blueprints/$1.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hst:hst/hst:blueprints/subsite/subtree
         nodePatterns = new String[] {"/hst:hst/hst:blueprints/" + NAME + "/" + NAME, "/hst:hst/hst:blueprints/" + NAME + "/" + NAME + "/" + ANY};
         propertyPatterns = new String[] {"/hst:hst/hst:blueprints/" + NAME + "/" + NAME + "/" + NAME, "/hst:hst/hst:blueprints/" + NAME + "/" + NAME + "/" + NAME + "/" + ANY};
         contextNode = "/hst:hst/hst:blueprints/$1/$2";
         file = "hst/blueprints/$1/$2.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hippo:configuration
         nodePatterns = new String[] {"/hippo:configuration", "/hippo:configuration/" + NAME};
         propertyPatterns = new String[] {"/hippo:configuration/" + NAME, "/hippo:configuration/" + NAME + "/" + NAME};
         contextNode = "/hippo:configuration";
         file = "configuration.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hippo:configuration/hippo:queries
         nodePatterns = new String[] {"/hippo:configuration/hippo:queries/" + NAME};
         propertyPatterns = new String[] {"/hippo:configuration/hippo:queries/" + NAME + "/" + NAME};
         contextNode = "/hippo:configuration/hippo:queries/$1";
         file = "configuration/queries/$1.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hippo:configuration/hippo:queries/queryfolder/query
         nodePatterns = new String[] {"/hippo:configuration/hippo:queries/" + NAME + "/" + NAME, "/hippo:configuration/hippo:queries/" + NAME + "/" + NAME + "/" + ANY};
         propertyPatterns = new String[] {"/hippo:configuration/hippo:queries/" + NAME + "/" + NAME + "/" + ANY};
         contextNode = "/hippo:configuration/hippo:queries/$1/$2";
         file = "configuration/queries/$1/$2.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /hippo:configuration/subnode/subsubnode
         nodePatterns = new String[] {"/hippo:configuration/" + NAME + "/" + NAME, "/hippo:configuration/" + NAME + "/" + NAME + "/" + ANY};
         propertyPatterns = new String[] {"/hippo:configuration/" + NAME + "/" + NAME + "/" + ANY};
         contextNode = "/hippo:configuration/$1/$2";
         file = "configuration/$1/$2.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /content/taxonomies
         nodePatterns = new String[] {"/content/taxonomies" + ANY};
         propertyPatterns = nodePatterns;
         contextNode = "/content/taxonomies";
         file = "taxonomies.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /content
         nodePatterns = new String[] {"/content", "/content/" + NAME};
         propertyPatterns = new String[] {"/content/" + NAME, "/content/" + NAME + "/" + NAME};
         contextNode = "/content";
         file = "content.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /content/documents/myproject
         nodePatterns = new String[] {"/content/" + NAME + "/" + NAME};
         propertyPatterns = new String[] {"/content/" + NAME + "/" + NAME + "/" + NAME};
         contextNode = "/content/$1/$2";
         file = "content/$1/$2.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // /content/documents/myproject/common
         nodePatterns = new String[] {"/content/" + NAME + "/" + NAME + "/" + NAME, "/content/" + NAME + "/" + NAME + "/" + NAME + "/" + ANY};
         propertyPatterns = new String[] {"/content/" + NAME + "/" + NAME + "/" + NAME + "/" + ANY};
         contextNode = "/content/$1/$2/$3";
         file = "content/$1/$2/$3.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // catch all: /node
         nodePatterns = new String[] {"/" + NAME, "/" + NAME + "/" + NAME};
         propertyPatterns = new String[] {"/" + NAME + "/" + NAME, "/" + NAME + "/" + NAME + "/" + NAME};
         contextNode = "/$1";
         file = "$1.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
         // catch all: /node/subnode/subsubnode
         nodePatterns = new String[] {"/" + NAME + "/" + NAME + "/" + NAME, "/" + NAME + "/" + NAME + "/" + NAME + "/" + ANY};
         propertyPatterns = new String[] {"/" + NAME + "/" + NAME + "/" + NAME + "/" + ANY};
         contextNode = "/$1/$2/$3";
         file = "$1/$2/$3.xml";
-        ENTRIES.add(new Entry(nodePatterns, propertyPatterns, contextNode, file));
+        ENTRIES.add(createEntry(nodePatterns, propertyPatterns, contextNode, file));
+    }
+
+    protected static Entry createEntry(String[] nodePatterns, String[] propertyPatterns, String contextNode, String file) {
+        return new Entry(nodePatterns, propertyPatterns, contextNode, file);
     }
 
     protected List<Entry> getEntries() {
@@ -171,7 +176,7 @@ public class InternalLocationMapperEntries {
         protected final String contextNode;
         protected final String file;
 
-        protected Entry(String[] nodePatterns, String[] propertyPatterns, String contextNode, String file) {
+        private Entry(String[] nodePatterns, String[] propertyPatterns, String contextNode, String file) {
             this.nodePatterns = new Pattern[nodePatterns.length];
             for (int i = 0; i < nodePatterns.length; i++) {
                 this.nodePatterns[i] = Pattern.compile(nodePatterns[i]);
